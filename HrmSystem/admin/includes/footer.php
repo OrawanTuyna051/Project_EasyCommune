e0f55fbe72f6d0d4c670b142e317572329c45647
OrawanTuyna051 <71110480+OrawanTuyna051@users.noreply.github.com>
2021-11-30 22:39:54 +0700

    Add files via upload

---
+++ HrmSystem/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>@<a href="#">Mustafa</a></b>
+    </div>
+    <strong>Copyright &copy; 2018 HRM, Attendance and Payroll System </strong>
+</footer>
